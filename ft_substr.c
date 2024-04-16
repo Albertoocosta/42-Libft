af1109963cb157fbfdb807d39efa7445538b4042
Carlos Alberto Da Fonseca Costa Neto <cda-fons@c1r11s1.42porto.com>
2024-04-16 20:11:24 +0100

    FEAT: create strjoin/substr

---
+++ ft_substr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cda-fons <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 18:10:41 by cda-fons          #+#    #+#             */
+/*   Updated: 2024/04/16 20:08:35 by cda-fons         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	i = 0;
+	if (start == 0)
+		return (NULL);
+	if (start > ft_strlen((char *)s))
+		return (NULL);
+	sub = malloc(sizeof(char) * (len + 1));
+	if (sub == 0)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
