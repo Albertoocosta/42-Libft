823596aee470fafa480aded7f7dd3c37007f2522
Carlos Alberto Da Fonseca Costa Neto <cda-fons@c2r4s5.42porto.com>
2024-05-08 16:45:04 +0100

    LIBFT 125%

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: cda-fons <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 19:38:39 by cda-fons          #+#    #+#             */
-/*   Updated: 2024/04/16 17:14:28 by cda-fons         ###   ########.fr       */
+/*   Updated: 2024/05/08 12:58:28 by cda-fons         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,13 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*ps1;
-	const unsigned char	*ps2;
-	size_t				i;
-	int					result;
+	size_t	i;
 
 	i = 0;
-	result = 0;
-	ps1 = (const unsigned char *)s1;
-	ps2 = (const unsigned char *)s2;
 	while (i < n)
 	{
-		if (ps1[i] != ps2[i])
-		{
-			result = ps1[i] - ps2[i];
-			if (result > 1)
-				return (1);
-			else if (result < 1)
-				return (-1);
-		}
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
 	return (0);
